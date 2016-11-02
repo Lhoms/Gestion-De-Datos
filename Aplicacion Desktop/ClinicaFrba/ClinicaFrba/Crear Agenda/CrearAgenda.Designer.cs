811c0a80e5fc178c8ad1c2a1d8f7e847afba490b
lhoms <lucashoms@gmail.com>
2016-11-02 20:42:06 -0300

    empiezo con agenda

--- Aplicacion Desktop/ClinicaFrba/ClinicaFrba/Crear Agenda/CrearAgenda.Designer.cs
+++ Aplicacion Desktop/ClinicaFrba/ClinicaFrba/Crear Agenda/CrearAgenda.Designer.cs
@@ -28,11 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimeDesde = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeHasta = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimeHasta);
+            this.groupBox1.Controls.Add(this.dateTimeDesde);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 122);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Duracion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta:";
+            // 
+            // dateTimeDesde
+            // 
+            this.dateTimeDesde.Location = new System.Drawing.Point(87, 19);
+            this.dateTimeDesde.Name = "dateTimeDesde";
+            this.dateTimeDesde.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeDesde.TabIndex = 2;
+            // 
+            // dateTimeHasta
+            // 
+            this.dateTimeHasta.Location = new System.Drawing.Point(87, 47);
+            this.dateTimeHasta.Name = "dateTimeHasta";
+            this.dateTimeHasta.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeHasta.TabIndex = 3;
+            // 
+            // CrearAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "CrearAgenda";
+            this.ClientSize = new System.Drawing.Size(596, 443);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "CrearAgenda";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Crear agenda";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimeHasta;
+        private System.Windows.Forms.DateTimePicker dateTimeDesde;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
